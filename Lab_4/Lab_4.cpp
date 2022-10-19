aed96c7db8f0f76bab828b55a3dc6aafce8d3a04
Mouz24 <vadim.dontsu@mail.ru>
2022-10-19 23:21:29 +0300

    Done

---
+++ Lab_4/Lab_4.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class Product
+{
+protected:
+	string type;
+
+public:
+	Product()
+	{
+
+	}
+
+	Product(string _type)
+	{
+		type = _type;
+	}
+
+
+	virtual ~Product() {}
+
+	virtual void PrintData() = 0;
+	virtual void GetPrice() = 0;
+};
+
+class Laptop : public virtual Product
+{
+protected:
+	int year;
+
+public:
+
+	Laptop()
+	{
+
+	}
+
+	Laptop(int _year, string _type)
+	{
+		type = _type;
+		year = _year;
+	}
+
+	void PrintData() override
+	{
+		cout << type << endl;
+		cout << year << endl;
+	}
+
+	void GetPrice() override
+	{
+		cout << setprecision(6) << (year / 23. * 1.2) << endl;
+	}
+
+	~Laptop() {}
+};
+
+class TV : public virtual Product {
+protected:
+	int width;
+
+public:
+	TV(int _width, string _type)
+	{
+		type = _type;
+		width = _width;
+	}
+
+	void PrintData() override
+	{
+		cout << type << endl;
+		cout << width << endl;
+	}
+
+	void GetPrice() override
+	{
+		cout << setprecision(4) << (width / 23. * 1.2) << endl;
+	}
+
+	~TV() {}
+};
+
+class IFlip : public Laptop
+{
+protected:
+	int autonomy;
+public:
+	IFlip(int _autonomy, int _year, string _type) : Laptop(_year, _type)
+	{
+		autonomy = _autonomy;
+	}
+
+	void PrintData() override
+	{
+		std::cout << type << std::endl;
+		std::cout << year << std::endl;
+		std::cout << autonomy << std::endl;
+	}
+
+	void GetPrice() override
+	{
+		cout << setprecision(6) << (double)(autonomy / 23. * 1.2) << endl;
+	}
+
+	~IFlip()
+	{
+
+	}
+};
+
+int main()
+{
+	Product** array = new Product * [3];
+	array[0] = new Laptop(2021, "Portable");
+		array[1] = new TV(123, "Mini");
+		array[2] = new IFlip(4500, 2025, "Max");
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Data : "; array[i]->PrintData();
+		cout << endl;
+		cout << "Price : "; array[i]->GetPrice();
+		cout << endl;
+	}
+	return 0;
+}
---
+++ Lab_5/Lab_5.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template<class T>
+class KeyValuePair
+{
+
+public:
+     static string Key;
+     T Value;
+
+    KeyValuePair()
+    {
+
+    }
+
+    KeyValuePair(string key, T value)
+    {
+        Value = value;
+        Key = key;
+    }
+};
+
+template<class T>
+T BinarySearch(string key, vector<class KeyValuePair<T>> arr, int count)
+{
+    int Min = 0;
+    int Max = count - 1;
+    int mid = (Min + Max) / 2;
+    KeyValuePair<T> element = arr[mid];
+    T value;
+    for (KeyValuePair<T> el : arr)
+    {
+        if (!el.Key.compare(key))
+        {
+            value = el.Value;
+        }
+    }
+    while (Min <= Max)
+    {
+        if (element.Value == value)
+        {
+            return mid + 1;
+        }
+        else if (element.Value < value)
+        {
+            Min = mid + 1;
+            mid = Min;
+            element = arr[mid];
+        }
+        else
+        {
+            Max = mid - 1;
+            mid = Max;
+            element = arr[mid];
+        }
+    }
+
+}
+
+int main()
+{
+    vector<KeyValuePair<int>> arr;
+
+    int count;
+    cin >> count;
+
+    for (int i = 0; i < count; i++)
+    {
+        string tempKey;
+        cin >> tempKey;
+
+        int value;
+        cin >> value;
+
+        KeyValuePair<int> temp(tempKey, value);
+        arr.push_back(temp);
+    }
+
+    string key;
+    cin >> key;
+
+    cout << BinarySearch(key, arr, count);
+    return 0;
+}
